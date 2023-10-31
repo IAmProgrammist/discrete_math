552d5b1cbc87eee5f1bb9f496b32cdc21c33158c
IAmProgrammist <vladislav.pakhomov@bk.ru>
2023-11-01 00:21:39 +0300

    Antisymmetric predicate algorithm fix

--- libs/alg/lab7/task23.cpp
+++ libs/alg/lab7/task23.cpp
@@ -62,7 +62,7 @@ bool BoolMatrixRelation::isAntiSymmetric(std::pair<int, int> &failed)
 {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            if (data[i][j] && data[j][i]) {
+            if ((data[i][j] && data[j][i]) && (i != j)) {
                 failed = {i + 1, j + 1};
                 return false;
             }
