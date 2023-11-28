2591afab3a2a5a50c727dd5dabc5fc9b3d697dd6
IAmProgrammist <vladislav.pakhomov@bk.ru>
2023-11-28 19:25:21 +0300

    Lab 4.1 task 3 completed

--- lab11/task3/main.cpp
+++ lab11/task3/main.cpp
@@ -69,76 +69,53 @@ Graph<Edge<IntNode>>* constructGraph2() {
     return G2;
 }
 
+void testRoute(Graph<Edge<IntNode>>* G, std::string graphName, std::vector<int>& route) {
+    std::cout << "==================================\n";
+    std::cout << "----------------------------------\n\n";
+    std::cout << "Testing route {";
+    for (int i = 0; i < route.size(); i++) {
+        if (i == route.size() - 1) {
+            std::cout << route[i];
+        } else {
+            std::cout << route[i] << ", ";
+        }
+    }
+    std::cout << "} for graph " << graphName << ":\n\n";
+    std::vector<IntNode*> nodes;
+    for (auto &element : route) {
+        nodes.push_back((*G)[element]);
+    }
+
+    std::cout << (G->isRoute(nodes) ? "Is route\n" : "Is not route\n");
+    std::cout << (G->isChain(nodes) ? "Is chain\n" : "Is not chain\n");
+    std::cout << (G->isSimpleChain(nodes) ? "Is simple chain\n" : "Is not simple chain\n");
+    std::cout << (G->isCycle(nodes) ? "Is cycle\n" : "Is not cycle\n");
+    std::cout << (G->isSimpleCycle(nodes) ? "Is simple cycle\n" : "Is not simple cycle\n\n");
+    std::cout << "----------------------------------\n";
+    std::cout << "==================================\n\n";
+}
+
 int main() {
     Graph<Edge<IntNode>> *G1 = constructGraph1();
     Graph<Edge<IntNode>> *G2 = constructGraph2();
 
-    std::cout << "G1 props:\n";
-    std::cout << "Is route?:\n";
-    std::cout << G1->isRoute({(*G1)[2], (*G1)[3], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6]}) << std::endl;
-    std::cout << G1->isRoute({(*G1)[4], (*G1)[1], (*G1)[6], (*G1)[7], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << G1->isRoute({(*G1)[1], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isRoute({(*G1)[1], (*G1)[2], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isRoute({(*G1)[2], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << "Is chain?:\n";
-    std::cout << G1->isChain({(*G1)[2], (*G1)[3], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6]}) << std::endl;
-    std::cout << G1->isChain({(*G1)[4], (*G1)[1], (*G1)[6], (*G1)[7], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << G1->isChain({(*G1)[1], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isChain({(*G1)[1], (*G1)[2], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isChain({(*G1)[2], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << "Is simple chain?:\n";
-    std::cout << G1->isSimpleChain({(*G1)[2], (*G1)[3], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6]}) << std::endl;
-    std::cout << G1->isSimpleChain({(*G1)[4], (*G1)[1], (*G1)[6], (*G1)[7], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << G1->isSimpleChain({(*G1)[1], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isSimpleChain({(*G1)[1], (*G1)[2], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isSimpleChain({(*G1)[2], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << "Is cycle?:\n";
-    std::cout << G1->isCycle({(*G1)[2], (*G1)[3], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6]}) << std::endl;
-    std::cout << G1->isCycle({(*G1)[4], (*G1)[1], (*G1)[6], (*G1)[7], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << G1->isCycle({(*G1)[1], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isCycle({(*G1)[1], (*G1)[2], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isCycle({(*G1)[2], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << "Is simple cycle?:\n";
-    std::cout << G1->isSimpleCycle({(*G1)[2], (*G1)[3], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6]}) << std::endl;
-    std::cout << G1->isSimpleCycle({(*G1)[4], (*G1)[1], (*G1)[6], (*G1)[7], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << G1->isSimpleCycle({(*G1)[1], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isSimpleCycle({(*G1)[1], (*G1)[2], (*G1)[4], (*G1)[3], (*G1)[2], (*G1)[1]}) << std::endl;
-    std::cout << G1->isSimpleCycle({(*G1)[2], (*G1)[4], (*G1)[1], (*G1)[7], (*G1)[6], (*G1)[1], (*G1)[2]}) << std::endl;
-    std::cout << "\n\n";
+    std::vector<int> route1 {2, 3, 4, 1, 7, 6};
+    std::vector<int> route2 {4, 1, 6, 7, 1, 2};
+    std::vector<int> route3 {1, 4, 3, 2, 1};
+    std::vector<int> route4 {1, 2, 4, 3, 2, 1};
+    std::vector<int> route5 {2, 4, 1, 7, 6, 1, 2};
 
-    
+    testRoute(G1, "G1", route1);
+    testRoute(G1, "G1", route2);
+    testRoute(G1, "G1", route3);
+    testRoute(G1, "G1", route4);
+    testRoute(G1, "G1", route5);
 
-    std::cout << "G2 props:\n";
-    std::cout << "Is route?:\n";
-    std::cout << G2->isRoute({(*G2)[2], (*G2)[3], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6]}) << std::endl;
-    std::cout << G2->isRoute({(*G2)[4], (*G2)[1], (*G2)[6], (*G2)[7], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << G2->isRoute({(*G2)[1], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isRoute({(*G2)[1], (*G2)[2], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isRoute({(*G2)[2], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << "Is chain?:\n";
-    std::cout << G2->isChain({(*G2)[2], (*G2)[3], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6]}) << std::endl;
-    std::cout << G2->isChain({(*G2)[4], (*G2)[1], (*G2)[6], (*G2)[7], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << G2->isChain({(*G2)[1], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isChain({(*G2)[1], (*G2)[2], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isChain({(*G2)[2], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << "Is simple chain?:\n";
-    std::cout << G2->isSimpleChain({(*G2)[2], (*G2)[3], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6]}) << std::endl;
-    std::cout << G2->isSimpleChain({(*G2)[4], (*G2)[1], (*G2)[6], (*G2)[7], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << G2->isSimpleChain({(*G2)[1], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isSimpleChain({(*G2)[1], (*G2)[2], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isSimpleChain({(*G2)[2], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << "Is cycle?:\n";
-    std::cout << G2->isCycle({(*G2)[2], (*G2)[3], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6]}) << std::endl;
-    std::cout << G2->isCycle({(*G2)[4], (*G2)[1], (*G2)[6], (*G2)[7], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << G2->isCycle({(*G2)[1], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isCycle({(*G2)[1], (*G2)[2], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isCycle({(*G2)[2], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << "Is simple cycle?:\n";
-    std::cout << G2->isSimpleCycle({(*G2)[2], (*G2)[3], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6]}) << std::endl;
-    std::cout << G2->isSimpleCycle({(*G2)[4], (*G2)[1], (*G2)[6], (*G2)[7], (*G2)[1], (*G2)[2]}) << std::endl;
-    std::cout << G2->isSimpleCycle({(*G2)[1], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isSimpleCycle({(*G2)[1], (*G2)[2], (*G2)[4], (*G2)[3], (*G2)[2], (*G2)[1]}) << std::endl;
-    std::cout << G2->isSimpleCycle({(*G2)[2], (*G2)[4], (*G2)[1], (*G2)[7], (*G2)[6], (*G2)[1], (*G2)[2]}) << std::endl;
+    testRoute(G2, "G2", route1);
+    testRoute(G2, "G2", route1);
+    testRoute(G2, "G2", route1);
+    testRoute(G2, "G2", route1);
+    testRoute(G2, "G2", route1);
 
     return 0;
 }
